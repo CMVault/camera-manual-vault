79b44406a5b7498988dcde5827b99da110685d35
CMVault <maxtowers10@gmail.com>
2025-07-20 14:29:54 -0700

    delete test4

--- script.js
+++
@@ -1,9 +0,0 @@
-function checkPassword() {
-  const input = document.getElementById("passwordInput").value;
-  if (input === "vaultaccess") {
-    document.querySelector(".splash-container").style.display = "none";
-    document.getElementById("home").style.display = "block";
-  } else {
-    alert("Incorrect password");
-  }
-}
